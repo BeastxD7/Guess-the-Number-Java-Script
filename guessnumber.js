f22b99628edf2e57125aa95806dc35031b20ec6b
BeastxD7 <shashank.21ai047@sode-edu.in>
2023-04-09 14:23:59 +0530

    Guess the Number |JavaScript

---
+++ guessnumber.js
@@ -0,0 +1,33 @@
+// Guess the Number
+let number = Math.random() * 100
+number = parseInt(number)
+console.log(number)
+var score = 101,chance = 0;
+let guess;
+do {
+  guess = prompt("Guess the Number")
+  guess = parseInt(guess)
+    if(guess > 100 || guess < 0)
+    {
+      console.log("Enter the Valid Number.")
+    }
+  else if(guess > number)
+  {
+    console.log(guess+ " is greater than the Random Number\n")
+  }
+  else if(guess < number)
+  {
+    console.log(guess+ " is less than the Random Number\n")
+  }
+  
+  chance++;
+  score--;
+  if (guess != number) {
+    console.log('Try again, Attempt: '+chance)
+  }
+  else {
+    console.log("Hurray!! You Guessed the Correct Number in "+chance+ " chances \nScore: "+score)
+  }
+}
+while (guess != number) 
+
